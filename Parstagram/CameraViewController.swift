2cbe54e6cbb15e161d5d542510800c0f48be5ba2
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-03-13 16:59:33 -0500

    Querying Posts

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
                             UINavigationControllerDelegate {
@@ -15,6 +16,24 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet weak var commentField: UITextField!
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
@@ -34,6 +53,12 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
